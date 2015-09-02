d3a6b43dc4aa3d2912e2985148786a14d8e67a21
Viktor Somodi <somodi.viktor@gmail.com>
2015-09-02 13:25:54 +0200

    #isOn #isOff #createForApiResponse

--- index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-class Flipper {
-
-}
-
-module.exports = Flipper;
--- test/flipper.spec.js
+++ test/flipper.spec.js
@@ -4,15 +4,91 @@ var Flipper = require('..');
 
 describe('Flipper', function() {
 
+  var flipper;
+
   describe('#isOn', function() {
 
-    it('dummy', function() {
-      this.expect(1).to.eql(1);
+    beforeEach(function() {
+      flipper = Flipper.create(['bi', 'dummy', 'dummy2']);
+    });
+
+    it('should respond with true if given flipper is provided', function() {
+      this.expect(flipper.isOn('bi')).to.be.true;
+    });
+
+
+    it('should respond with false if given flipper is not provided', function() {
+      this.expect(flipper.isOn('predict')).to.be.false;
     });
 
   });
 
   describe('#isOff', function() {
+
+    beforeEach(function() {
+      flipper = Flipper.create(['bi', 'dummy', 'dummy2']);
+    });
+
+    it('should respond with false if given flipper is provided', function() {
+      this.expect(flipper.isOff('bi')).to.be.false;
+    });
+
+
+    it('should respond with true if given flipper is not provided', function() {
+      this.expect(flipper.isOff('predict')).to.be.true;
+    });
+
+  });
+
+  describe('Factories', function() {
+
+    describe('#createForApiResponse', function() {
+
+      beforeEach(function() {
+        flipper = Flipper.createForApiResponse([
+          { id: 'bi', isOn: true },
+          { id: 'dummy', isOn: true },
+          { id: 'dummy2', isOn: false }
+        ]);
+      });
+
+      describe('#isOn', function() {
+
+        it('should respond with true if given flipper is provided with isOn', function() {
+          this.expect(flipper.isOn('bi')).to.be.true;
+        });
+
+
+        it('should respond with false if given flipper is not provided', function() {
+          this.expect(flipper.isOn('predict')).to.be.false;
+        });
+
+
+        it('should respond with false if given flipper is provided with isOn false', function() {
+          this.expect(flipper.isOn('dummy2')).to.be.false;
+        });
+
+      });
+
+      describe('#isOff', function() {
+
+        it('should respond with true if given flipper is provided with isOff', function() {
+          this.expect(flipper.isOff('dummy2')).to.be.true;
+        });
+
+
+        it('should respond with true if given flipper is not provided', function() {
+          this.expect(flipper.isOff('predict')).to.be.true;
+        });
+
+
+        it('should respond with false if given flipper is provided with isOn true', function() {
+          this.expect(flipper.isOff('bi')).to.be.false;
+        });
+
+      });
+
+    });
 
   });
 
