cd04cca8bfbc58e265a4e506147c6b7880601b55
Viktor Somodi <somodi.viktor@gmail.com>
2015-09-02 12:45:12 +0200

    add test environment with dummy test

---
+++ test/flipper.spec.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var Flipper = require('..');
+
+describe('Flipper', function() {
+
+  describe('#isOn', function() {
+
+    it('dummy', function() {
+      this.expect(1).to.eql(1);
+    });
+
+  });
+
+  describe('#isOff', function() {
+
+  });
+
+});
